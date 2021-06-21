6411a52dce8a5ad3fd3c36771efbce5380cdafac
prohitman <43189387+prohitman@users.noreply.github.com>
2021-06-22 00:42:52 +0100

    Fixing rendering bugs

--- src/main/java/com/prohitman/untitledswanmod/events/client/ClientEventHandler.java
+++ src/main/java/com/prohitman/untitledswanmod/events/client/ClientEventHandler.java
@@ -2,7 +2,11 @@ package com.prohitman.untitledswanmod.events.client;
 
 import com.prohitman.untitledswanmod.UntitledSwanMod;
 import com.prohitman.untitledswanmod.client.renderer.SwanEntityRenderer;
+import com.prohitman.untitledswanmod.entity.SwanEggEntity;
 import com.prohitman.untitledswanmod.init.ModEntities;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.ItemRenderer;
+import net.minecraft.client.renderer.entity.SpriteRenderer;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
@@ -14,6 +18,9 @@ public class ClientEventHandler {
 
     @SubscribeEvent
     public static void clientSetup(FMLClientSetupEvent event) {
+        ItemRenderer itemRendererIn = Minecraft.getInstance().getItemRenderer();
+
         RenderingRegistry.registerEntityRenderingHandler(ModEntities.SWAN_ENTITY.get(), SwanEntityRenderer::new);
+        RenderingRegistry.registerEntityRenderingHandler(ModEntities.SWAN_EGG_ENTITY.get(),manager -> new SpriteRenderer(manager, itemRendererIn));
     }
 }
